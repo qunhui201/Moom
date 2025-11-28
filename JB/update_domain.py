a4a42eba4e33aa3b8d8f7cb3072d32beae56fac3
qunhui201 <qunhui201@outlook.com>
2025-11-28 23:59:56 +0800

    Update update_domain.py

--- JB/update_domain.py
+++ JB/update_domain.py
@@ -26,7 +26,7 @@ def main():
     valid_links = []
 
     # 这里可以调整范围，比如 8000-8099
-    for i in range(6600, 6699):
+    for i in range(600, 6799):
         domain = f"{i}ck.cc"
         url_to_test = base_url_template.format(domain=domain)
         print(f"Testing URL: {url_to_test}")
